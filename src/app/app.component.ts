22e1fe8a295ead25fe0101ca81c3a24d3ae2c5c0
foolcardssk <foolcardssk@gmail.com>
2023-10-15 18:44:38 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'd43-m0-n-ssk';
+}
